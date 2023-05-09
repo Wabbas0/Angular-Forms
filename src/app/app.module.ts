a7694018b94cdf53ded20ab1aed05c747a5f8699
waleed <waleed.gamal1000@gmail.com>
2023-05-09 08:50:20 +0200

    Generate Form component and render it in APP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field'; // Import the module
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatCardModule,
+    MatFormFieldModule, 
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/form/form.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
+interface Field {
+  field: string;
+  label: string;
+  type: 'text' | 'check';
+  hidden: 'true' | 'false';
+  mandatory?: boolean;
+  value?: string;
+}
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent implements OnInit {
+  form: FormGroup = new FormGroup({});
+  fields!: Field[];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get<Field[]>('assets/form.json').subscribe(data => {
+      console.log(data);
+      this.fields = data;
+      this.updateFormControls();
+    });
+  }
+
+  private updateFormControls() {
+    const controls: {[key: string]: FormControl} = {};
+    for (const field of this.fields) {
+      controls[field.field] = new FormControl(field.value || '', field.mandatory ? Validators.required : null);
+    }
+    this.form = new FormGroup(controls);
+  }
+
+  onSubmit() {
+    console.log(this.form.value);
+  }
+
+  onFieldsChanged() {
+    this.updateFormControls();
+  }}
